aa52b9e29ab5ba551d1f5789f5c08a2aad762f82
pinar-mat <63678861+pinar-mat@users.noreply.github.com>
2021-07-18 16:30:52 +0300

    Initial commit

---
+++ src/main/java/patuli/Docker_Test/controller/TestController.java
@@ -0,0 +1,16 @@
+package patuli.Docker_Test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @GetMapping
+    public  String getName(){
+        return "test Docker connection";
+    }
+}
---
+++ src/test/java/patuli/Docker_Test/DockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package patuli.Docker_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
